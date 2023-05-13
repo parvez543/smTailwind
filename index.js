89d46a218686b0de806977df7f0a68aaa6bec0ca
Parvez <parvez@studiometrodesk.com>
2023-05-13 17:27:05 +0600

    Form Flip Added

--- index.js
+++ index.js
@@ -1,25 +1,63 @@
 const form = document.getElementById('my-form');
 form.addEventListener('submit', function(event){
     event.preventDefault();
-    const name = event.target.name.value
-      const email = event.target.email.value
-    const phoneNumber = event.target.select.value + event.target.tel.value
-    const wantCallElement = event.target.radio.checked
-    
-    const services = event.target.service.value
-    const instruction = event.target.instruction.value
-    // const transfer = event.target.transfer.value
-    const user = {
-            name: name,
-            email: email,
-            phoneNumber: phoneNumber,
-            call: wantCallElement,
-            services: services,
-            instruction: instruction
-        }
-       
-  console.log(user)
+   
+
 })
+
+// Get form and loginButton elements
+const formDetails = document.getElementById("my-form");
+const loginButton = document.getElementById("loginButton");
+
+// Disable loginButton by default
+loginButton.disabled = true;
+
+// Add event listener to form
+form.addEventListener("input", () => {
+  // Check if all required fields have a value
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const service = document.getElementById("service").value;
+  const instruction = document.getElementById("instruction").value;
+
+  if (name && email && service && instruction) {
+    // Enable loginButton if all fields have a value
+    loginButton.disabled = false;
+    loginButton.classList.remove('disabled');
+    loginButton.classList.add('transferBtnActive');
+  } else {
+    // Disable loginButton if any required field is empty
+    loginButton.disabled = true;
+    loginButton.classList.add('disabled');
+    loginButton.classList.remove('transferBtnActive');
+  }
+});
+
+
+const registerButton = document.getElementById("registerButton");
+
+loginButton.addEventListener("click", function(){
+	document.querySelector("#flipper").classList.toggle("flip");
+})
+
+registerButton.addEventListener("click", function(){
+  document.querySelector("#flipper").classList.toggle("flip");
+})
+
+loginButton.addEventListener("click", function(){
+  document.getElementById('formText').style.display = "none";
+})
+registerButton.addEventListener("click", function(){
+  document.getElementById('formText').style.display = "block";
+})
+
+
+
+
+  
+ 
+
+
 
 
 
