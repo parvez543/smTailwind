dc23259c651c9f38218d55a28f14511586e7ad8d
Parvez <parvez@studiometrodesk.com>
2023-05-30 21:04:54 +0600

    Nav

--- index.js
+++ index.js
@@ -4281,5 +4281,16 @@ function typeText() {
 // Start typing the text when the page loads
 window.addEventListener("load", typeText);
 
-
+// Text Rotation Of Header
+setInterval(function () {
+  const show = document.querySelector('span[data-show]')
+  const next = show.nextElementSibling || document.querySelector('span:first-child')
+  const up = document.querySelector('span[data-up]')
+  if (up) {
+    up.removeAttribute('data-up')
+  }
+  show.removeAttribute('data-show')
+  show.setAttribute('data-up', '')
+  next.setAttribute('data-show', '')
+}, 2000)
 
